b9fe8378779fb3f0116227991041c99baeb014b7
Brandon McIntosh <Brandon.L.Mcintosh@gmail.com>
2021-03-15 18:46:58 -0700

    added actions as a db model

---
+++ models/actions.py
@@ -0,0 +1,15 @@
+from connect_db import db
+
+
+class Action(db.Model):
+    """Action"""
+
+    __tablename__ = 'actions'
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    organization = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    bill_id = db.Column(db.Integer, db.ForeignKey(
+        'bills.id'), nullable=False)
+    bill = db.relationship("Bill")
